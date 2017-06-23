8eaf044a647f6725dfeca8bbc7956f396bec6612
gilmoregrills <robinfarrowyonge@gmail.com>
2017-06-23 13:33:06 +0100

    add pca functions, including reconstruction, found format of input array is incorrect, branch to solve

--- implementation/individualiser/individualiser/utility_functions.py
+++ implementation/individualiser/individualiser/utility_functions.py
@@ -3,87 +3,118 @@ import scipy.io as sio
 import numpy as np
 import sklearn.decomposition as decomp
 
-#take database as input, prepare correct input matrix
-#var database is only 'cipic', 'ari', 'mit' etc
-#var as_hrtf, False produces restructured HRIR matrix
-#var all_participants is a boolean, if False produces input array of 1 subject
-#var two_dimensions is also a boolean
+# take database as input, prepare correct input matrix
+# var database is only 'cipic', 'ari', 'mit' etc
+# var as_hrtf, False produces restructured HRIR matrix
+# var all_participants is a boolean, if False produces input array of 1 subject
+# var two_dimensions is also a boolean
 def restructure_data(database, as_hrtf, all_participants):
-    #set the database path, atm only working with CIPIC
+    # set the database path, atm only working with CIPIC
     path = ""
     if database == "cipic" or database == "CIPIC":
         path = "../databases/CIPIC/CIPIC_hrtf_database/standard_hrir_database/"
     
-    #initialise all variables
-    output_matrix = []#[(subject * direction) * samples]
-    all_subjects = []#list of all subject data as matlab object thingies
-    subject_dirs = sorted(os.listdir(path))#subject directory names, chrono sorted
-    subject_dirs.remove("show_data")#remove the matlab scripts folder name
+    # initialise all variables
+    output_matrix = []# [(subject * direction) * samples]
+    all_subjects = []# list of all subject data as matlab object thingies
+    subject_dirs = sorted(os.listdir(path))# subject directory names, chrono sorted
+    subject_dirs.remove("show_data")# remove the matlab scripts folder name
     
-    #gather data for each subject, append to list
+    # gather data for each subject, append to list
     for sub_dir in subject_dirs: 
         subject = sio.loadmat(path+sub_dir+"/hrir_final.mat")
         all_subjects.append(subject) 
     
-    #if required, fft all data
+    # if required, fft all data
     if as_hrtf == True:
         for subject in all_subjects:
             subject['hrir_l'] = np.fft.rfft(subject['hrir_l'])
             subject['hrir_r'] = np.fft.rfft(subject['hrir_r'])
 
-    #now rearrange that data into the currect output_matrix structure
+    # now rearrange that data into the currect output_matrix structure
     if all_participants == True:
-        output_matrix = np.empty([len(all_subjects[0]['hrir_l'][0][0]), len(all_subjects[0]['hrir_l'])*len(all_subjects[0]['hrir_l'][0])*len(all_subjects)])
-
-        for sample in range(0, len(all_subjects[0]['hrir_l'][0][0])):
+        output_matrix = np.empty([len(all_subjects[0]['hrir_l'][0][0])*2, len(all_subjects[0]['hrir_l'])*len(all_subjects[0]['hrir_l'][0])*len(all_subjects)])
+        output_sample_index = 0
+        input_sample_index = 0
+        for sample in range(0, len(all_subjects[0]['hrir_l'][0][0]*2)): 
             counter = 0
             for azimuth in range(0, len(all_subjects[0]['hrir_l'])):
                 for elevation in range(0, len(all_subjects[0]['hrir_l'][0])):
                     for subject in range(0, len(all_subjects)): 
-                        output_matrix[sample][counter] = all_subjects[subject]['hrir_l'][azimuth][elevation][sample]
-                        #counter just provides a row index for inputmatrix
-                        counter = counter+1
+                        output_matrix[output_sample_index][counter] = all_subjects[subject]['hrir_l'][azimuth][elevation][input_sample_index]
+                        # counter just provides a row index for output_matrix
+                        counter += 1
+            output_sample_index += 1
+            counter = 0
+            for azimuth in range(0, len(all_subjects[0]['hrir_l'])):
+                for elevation in range(0, len(all_subjects[0]['hrir_l'][0])):
+                    for subject in range(0, len(all_subjects)): 
+                        output_matrix[output_sample_index][counter] = all_subjects[subject]['hrir_r'][azimuth][elevation][input_sample_index]
+                        # counter just provides a row index for output_matrix
+                        counter += 1
+            output_sample_index += 1
+            input_sample_index += 1
 
     # generate a 200*1250 2D array representing the mean value of one participant 
     elif all_participants == False:
-        output_matrix = np.empty([len(all_subjects[0]['hrir_l'][0][0]), len(all_subjects[0]['hrir_l'])*len(all_subjects[0]['hrir_l'][0])])
+        output_matrix = np.empty([len(all_subjects[0]['hrir_l'][0][0])*2, len(all_subjects[0]['hrir_l'])*len(all_subjects[0]['hrir_l'][0])])
+        output_sample_index = 0
+        input_sample_index = 0
         for sample in range(0, len(all_subjects[0]['hrir_l'][0][0])):
             counter = 0
             for azimuth in range(0, len(all_subjects[0]['hrir_l'])):
                 for elevation in range(0, len(all_subjects[0]['hrir_l'][0])):
                     tmp = np.empty([45]) # temporary variable for calculating the mean HRTF
                     for subject in range(0, len(all_subjects)): 
-                        tmp[subject] = all_subjects[subject]['hrir_l'][azimuth][elevation][sample]
-                    output_matrix[sample][counter] = np.mean(tmp)
-                    counter = counter+1
+                        tmp[subject] = all_subjects[subject]['hrir_l'][azimuth][elevation][input_sample_index]
+                    output_matrix[output_sample_index][counter] = np.mean(tmp)
+                    counter += 1
+            counter = 0
+            output_sample_index += 1
+            for azimuth in range(0, len(all_subjects[0]['hrir_l'])):
+                for elevation in range(0, len(all_subjects[0]['hrir_l'][0])):
+                    tmp = np.empty([45]) # temporary variable for calculating the mean HRTF
+                    for subject in range(0, len(all_subjects)): 
+                        tmp[subject] = all_subjects[subject]['hrir_r'][azimuth][elevation][input_sample_index]
+                    output_matrix[output_sample_index][counter] = np.mean(tmp)
+                    counter += 1
+            output_sample_index += 1
+            input_sample_index += 1
 
-
-
-
-    #returning subject data for testing scripts
+    # returning subject data for testing scripts
     return_list = [output_matrix, all_subjects]
     return return_list
 
-#PCA on an input Matrix, returning PCs, PCWs, etc
-def run_pca(output_matrix, components):
+# creates PCA object and trains/fits it
+def train_pca(input_matrix, components):
     pca = decomp.PCA(n_components=components)
-    pca.fit(output_matrix)
+    pca.fit(input_matrix)
     return pca
 
+# takes a trained PCA object/model, and an input matrix, and returns the PCs/features
+def pca_transform(pca_model, input_matrix):
+    output_matrix = pca_model.transform(input_matrix)
+    return output_matrix
 
-#model PCWs as spherical harmonics
+# takes a trained PCA model with a previously-transformed input matrix, and returns
+# a matrix matching the original input (the restructured HRTF)
+def pca_reconstruct(pca_model, input_matrix):
+    output_matrix = pca_model.inverse_transform(input_matrix)
+    return output_matrix
+
+# model PCWs as spherical harmonics
 def spher_harm(weights):
     return
 
 
-#reconstruct an HRTF from PCW etc
+# reconstruct an HRTF from PCW etc
 def reconstruct(weights, components, mean):
     return
 
 
-#uses the numpy.fft set of functions to transform input matrices 
-#if the inverse flag is true it calls the inverse fft, to go from
-#hrtf to hrir!
+# uses the numpy.fft set of functions to transform input matrices 
+# if the inverse flag is true it calls the inverse fft, to go from
+# hrtf to hrir!
 def fourier_transform(input_data, inverse):
     print "\nStarting FFT \nInput matrix dimension is:"
     print np.ndim(input_data)
@@ -92,7 +123,7 @@ def fourier_transform(input_data, inverse):
  
     if inverse == False:
         return_list = ["", ""]
-        return_list[1] = np.fft.rfftfreq(input_data.size, d=1./44100)#to be used to label axes?
+        return_list[1] = np.fft.rfftfreq(input_data.size, d=1./44100)# to be used to label axes?
         if np.ndim(input_data) == 1:
             print("\nperforming 1D fft\n")
             return_list[0] = np.fft.rfft(input_data)
--- implementation/individualiser/tests/pcaTest.py
+++ implementation/individualiser/tests/pcaTest.py
@@ -2,14 +2,15 @@ import sys
 sys.path.append('../')
 import individualiser.utility_functions as util
 import numpy as np
-
+import sklearn.decomposition as decomp
+"""
 print "Fetching PCA input data for all hrtfs"
 data = util.restructure_data('CIPIC', True, True)
 input_matrix = data[0]
 
 
 print "\nRunning PCA"
-pca = util.run_pca(input_matrix, 15)
+pca = util.run_pca(input_matrix, 24)
 
 print "\nNumber of components:"
 print pca.n_components_
@@ -17,19 +18,46 @@ print "\nExplained variance of each component:"
 print pca.explained_variance_ratio_
 print "\nTotal explained variance:"
 print np.sum(pca.explained_variance_ratio_)
+"""
 
-
-print "\nFetching PCA input data for single hrtf"
+print "\nFetching PCA input data for single hrtf\n"
 data = util.restructure_data('CIPIC', True, False)
 input_matrix = data[0]
-
+print "\nOriginal input hrtf shape:"
+print input_matrix.shape
 
 print "\nRunning PCA"
-pca = util.run_pca(input_matrix, 9)
+pca = decomp.PCA(n_components=10)
+pca.fit(input_matrix)
+#pca = util.train_pca(input_matrix, 10)# create and train 10 component pca model
+hrtf_pcs = pca.transform(input_matrix)
+#hrtf_pcs = util.pca_transform(input_matrix)
 
+print "\nAbout the trained model:"
+print "\nShape of pca object components_:"
+print pca.components_.shape
 print "\nNumber of components:"
 print pca.n_components_
 print "\nExplained variance of each component:"
+print pca.explained_variance_
+print "\nExplained variance ratio of each component:"
 print pca.explained_variance_ratio_
 print "\nTotal explained variance:"
 print np.sum(pca.explained_variance_ratio_)
+
+print "\nAbout the transformed input matrix:"
+print "\nPre-transform:"
+print input_matrix.shape
+print "\nPost-transform:"
+print "shape:"
+print hrtf_pcs.shape
+print "number of components:"
+print len(hrtf_pcs)
+print "each component array length:"
+print len(hrtf_pcs[0])
+
+print "\nReconstruction:"
+reconstructed_hrtf = pca.inverse_transform(hrtf_pcs)
+print "\nReconstructed HRTF shape:"
+print reconstructed_hrtf.shape
+#maybe generate graph here
