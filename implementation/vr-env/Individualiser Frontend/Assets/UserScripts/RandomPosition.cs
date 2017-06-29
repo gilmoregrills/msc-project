dff23fd0836bf36c7dffa46cd4765169fe970f96
gilmoregrills <robinfarrowyonge@gmail.com>
2017-06-29 18:20:29 +0100

    add scripts to record vectors and set audio source position

---
+++ implementation/vr-env/Individualiser Frontend/Assets/UserScripts/RandomPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPosition : MonoBehaviour {
+	GameObject audioSource;
+	Vector3 currentPosition;
+	Quaternion currentRotation;
+	Transform transform;
+	Transform newTransform;
+	// Use this for initialization
+	void Start () {
+		audioSource = GameObject.Find ("Source1");
+		Random rnd = new Random ();
+		audioSource.transform.SetPositionAndRotation((Random.insideUnitSphere.normalized * 10), currentRotation);
+		audioSource.transform.LookAt (new Vector3 (0, 0, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("z") | Input.GetKeyDown("left alt") && Input.GetKeyDown("z")) {
+			print ("pressed Z - generating new random position");
+			currentPosition = audioSource.transform.position;
+			currentRotation = audioSource.transform.rotation;
+			print ("current position = " + currentPosition);
+			audioSource.transform.SetPositionAndRotation((Random.insideUnitSphere.normalized * 10), currentRotation);
+			audioSource.transform.LookAt (new Vector3 (0, 0, 0));
+			//play audio sample
+		}
+	}
+}
---
+++ implementation/vr-env/Individualiser Frontend/Assets/UserScripts/TransmitVector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransmitVector : MonoBehaviour {
+	
+	Vector3 userToSource;
+	Vector3 userLocalisation;
+	GameObject user;
+	GameObject source;
+	GameObject reticle;
+
+	// Use this for initialization
+	void Start () {
+		user = GameObject.Find("MegaCam");
+		source = GameObject.Find("Source1");
+		reticle = GameObject.Find("Ball");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("x") | Input.GetKeyDown("left alt") && Input.GetKeyDown("x")) {
+			print ("pressed X, transmitting perceived/actual source vector");
+			userToSource = source.transform.position - user.transform.position;
+			userLocalisation = reticle.transform.position - user.transform.position;
+			print ("Actual vector from user to sound source =" + userToSource.ToString ());
+			print ("Perceived vector from user to sound source =" + userLocalisation.ToString ());
+		}
+			//calculate the current vector, send
+			//that information to listening socket
+			//in the individualiser module
+	}
+}
