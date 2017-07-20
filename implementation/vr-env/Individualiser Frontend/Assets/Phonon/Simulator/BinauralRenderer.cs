763033975a32060ea25b4d24072656ecb8cc0d9b
gilmoregrills <robinfarrowyonge@gmail.com>
2017-07-20 12:20:12 +0100

    closer than ever before to custom hrtfs in unity

--- implementation/vr-env/Individualiser Frontend/Assets/Phonon/Simulator/BinauralRenderer.cs
+++ implementation/vr-env/Individualiser Frontend/Assets/Phonon/Simulator/BinauralRenderer.cs
@@ -2,28 +2,16 @@
 // Copyright (C) Valve Corporation. All rights reserved.
 //
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Text;
 using UnityEngine;
-using System.Collections;
 namespace Phonon
 {
     public class BinauralRenderer
     {
-		System.Net.Sockets.TcpClient clientSocket;
-		NetworkStream serverStream;
-        Int32 size;
-		byte[] inputData;
-        byte[] sizeData;
-		string asString;
-		public double[][][] leftEarHrtfs;
-		public double[][][] rightEarHrtfs;
-        public double[][][][] fullHrtf;
-        public double[] directions;
-        GameObject connectNotif;
-        GameObject fetchNotif;
-
         public void Create(Environment environment, RenderingSettings renderingSettings, GlobalContext globalContext)
         {
             HRTFParams hrtfParams = new HRTFParams
@@ -41,6 +29,8 @@ namespace Phonon
             var error = PhononCore.iplCreateBinauralRenderer(globalContext, renderingSettings, hrtfParams, ref binauralRenderer);
             if (error != Error.None)
                 throw new Exception("Unable to create binaural renderer [" + error.ToString() + "]");
+
+            //could I initialise all my instance variables in here??
         }
 
 		public IntPtr GetBinauralRenderer()
@@ -54,21 +44,32 @@ namespace Phonon
                 PhononCore.iplDestroyBinauralRenderer(ref binauralRenderer);
         }
 
-        public void onLoadHrtf(int numSamples, int numSpectrumSamples, Phonon.FFTHelper fft, System.IntPtr fftHelperData)
+        public void onLoadHrtf(int numSamples, int numSpectrumSamples, Phonon.FFTHelper fft, System.IntPtr data)
         {
+            Stopwatch timer = new Stopwatch();
+            //test variables
+            System.Runtime.Serialization.Formatters.Binary.BinaryFormatter bf = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+
+
+            //declaring variables in tighter scope
+            double[][][][] fullHrtf;
+            TcpClient clientSocket;
+            NetworkStream serverStream;
+            Int32 size;
+            byte[] inputData;
+            byte[] sizeData;
+            string asString;
+
             sizeData = new byte[24];
             clientSocket = new System.Net.Sockets.TcpClient();
 
             clientSocket.Connect("35.176.144.147", 54679); //if running in editor, connect localhost
-            Debug.Log("connecting to local version");
+            UnityEngine.Debug.Log("connecting to local version");
 
             if (clientSocket.Connected)
             {
-                Debug.Log("connection made");
-                connectNotif = GameObject.Find("Connected");
-                UnityEngine.Vector3 pos = connectNotif.transform.position;
-                pos.x = 0;
-                connectNotif.transform.position = pos;
+                UnityEngine.Debug.Log("connection made");
             }
             serverStream = clientSocket.GetStream();
             serverStream.Read(sizeData, 0, 24);
@@ -76,13 +77,15 @@ namespace Phonon
             asString = System.Text.Encoding.Default.GetString(sizeData);
 
             size = Int32.Parse(asString);
-            Debug.Log("size value = " + size);
+            UnityEngine.Debug.Log("size value = " + size);
             StringBuilder wip = new StringBuilder();
             inputData = new byte[1024];
-            int numBytesRead = 0;
+            //int numBytesRead = 0;
             clientSocket.ReceiveTimeout = 1000;
             serverStream.ReadTimeout = 1000;
-            do
+            StreamReader reado = new StreamReader(serverStream);
+
+            /*do
             {
                 numBytesRead += serverStream.Read(inputData, 0, inputData.Length);
 
@@ -91,9 +94,10 @@ namespace Phonon
 
             } while (serverStream.DataAvailable);
             Debug.Log("bytes read: "+numBytesRead);
+            */
 
-            asString = wip.ToString();
-            Debug.Log("end of hrtf as string lol \n" + asString.Substring(5540000));
+            asString = reado.ReadToEnd(); //wip.ToString();
+            UnityEngine.Debug.Log("end of hrtf as string lol \n" + asString.Substring(5540000));
             int index = asString.IndexOf("]]]]") + 4;
             String doubles = asString.Substring(index);
             asString = asString.Substring(0, index);
@@ -105,40 +109,60 @@ namespace Phonon
             }
             */
             //Debug.Log("hrtf as string lol \n" + asString.Substring(5540000));
+            
+            fullHrtf = Newtonsoft.Json.JsonConvert.DeserializeObject<double[][][][]>(asString);
+            bf.Serialize(ms, fullHrtf);
+            byte[] biter = ms.ToArray();
+            /* THIS SHOULD BE USED WHEN FETCHING HRTFDATA AS A BYTE ARRAY FROM INTPTR, TAKES 14ms 
+            timer.Start();
+            MemoryStream mms = new MemoryStream(biter);
+            double[][][][] testo = (double[][][][])bf.Deserialize(mms);
+            timer.Stop();
+            UnityEngine.Debug.Log(timer.ElapsedMilliseconds);
+            UnityEngine.Debug.Log("dimensions of deserialised array: "+testo.Length+","+testo[0].Length+","+testo[0][0].Length+","+testo[0][0][0].Length);
+            */
+            UnityEngine.Debug.Log(data);
+            UnityEngine.Debug.Log(data.GetType());
+            //MOVE THIS HRTF DATA INTO THE INTPTR PROVIDED
+            Marshal.Copy(biter, 0, data, biter.Length);
 
-            this.fullHrtf = Newtonsoft.Json.JsonConvert.DeserializeObject<double[][][][]>(asString);
-            this.leftEarHrtfs = fullHrtf[0];
-            this.rightEarHrtfs = fullHrtf[1];
-            Debug.Log("hrtf shape: " + leftEarHrtfs.Length + ", " + leftEarHrtfs[0].Length + ", " + leftEarHrtfs[0][0].Length);
-            Debug.Log("hrtf shape: " + rightEarHrtfs.Length + ", " + rightEarHrtfs[24].Length + ", " + rightEarHrtfs[24][49].Length);
-            Debug.Log("HRTFs loaded");
-            fetchNotif = GameObject.Find("Fetched HRTF");
-            UnityEngine.Vector3 newPos = fetchNotif.transform.position;
-            newPos.x = -6;
-            fetchNotif.transform.position = newPos;
+            UnityEngine.Debug.Log("HRTFs loaded");
+            //COPY HRTF DATA TO HRTFDATA INTPTR, DONT HAVE ANYTHING AS AN INSTANCE VARIABLE
         }
 
 		public void onUnloadHrtf()
 		{
-            Debug.Log("unloading hrtf data");
+            UnityEngine.Debug.Log("");
+            UnityEngine.Debug.Log("unloading hrtf data");
             //remove everything from memory
-            Debug.Log(this.fullHrtf.Length);
-            leftEarHrtfs = null;
-            rightEarHrtfs = null;
+
+            //DESTROY THE HRTFDATA INTPTR
+            //this.leftEarHrtfs = null;
+            //this.rightEarHrtfs = null;
 
         }
 
 		public void onLookupHrtf(System.IntPtr direction, System.IntPtr leftHrtf, System.IntPtr rightHrtf)
 		{
-            Debug.Log("Fetching HRTF for direction");
-            Debug.Log(direction.ToString());
-            Debug.Log(direction.GetType());
-            this.directions = new double[3];
+            //ONCE I FIND HOW TO GET THE HrtfData IntPtr I can get its size with IntPtr.size and do this stuff below:
+            //System.Runtime.Serialization.Formatters.Binary.BinaryFormatter bf = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            //byte[] biter = new byte[size];
+            //Marshal.Copy(HRTFData, biter, 0, size);
+            //MemoryStream mms = new MemoryStream(biter);
+            //double[][][][] fullHrtf = (double[][][][])bf.Deserialize(mms);
+            
+            double[] directions;
+            UnityEngine.Debug.Log(GetBinauralRenderer());
+            UnityEngine.Debug.Log("Fetching HRTF for direction");
+            //GRAB HRTF DATA FROM THE HRTFDATA INTPTR AND USE IT THAT WAY OMFG
+            UnityEngine.Debug.Log(direction.ToString());
+            UnityEngine.Debug.Log(direction.GetType());
+            directions = new double[3];
             Marshal.Copy(direction, directions, 0, directions.Length);
             //double x = directions[0];
             //double y = directions[1];
             //double z = directions[2];
-            Debug.Log(directions);
+            UnityEngine.Debug.Log(directions);
             //transform a vector into the correct hrtf direction
             //fetch that direction and stuff from the hrtf stuff?
 
