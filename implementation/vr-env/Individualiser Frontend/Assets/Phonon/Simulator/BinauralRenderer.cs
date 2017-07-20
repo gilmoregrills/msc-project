0d96ce77b63011097bd82550cae6bfb6dd77095c
gilmoregrills <robinfarrowyonge@gmail.com>
2017-07-20 15:03:26 +0100

    made slight progress with test environment

--- implementation/vr-env/Individualiser Frontend/Assets/Phonon/Simulator/BinauralRenderer.cs
+++ implementation/vr-env/Individualiser Frontend/Assets/Phonon/Simulator/BinauralRenderer.cs
@@ -12,6 +12,7 @@ namespace Phonon
 {
     public class BinauralRenderer
     {
+
         public void Create(Environment environment, RenderingSettings renderingSettings, GlobalContext globalContext)
         {
             HRTFParams hrtfParams = new HRTFParams
@@ -29,8 +30,6 @@ namespace Phonon
             var error = PhononCore.iplCreateBinauralRenderer(globalContext, renderingSettings, hrtfParams, ref binauralRenderer);
             if (error != Error.None)
                 throw new Exception("Unable to create binaural renderer [" + error.ToString() + "]");
-
-            //could I initialise all my instance variables in here??
         }
 
 		public IntPtr GetBinauralRenderer()
@@ -46,26 +45,20 @@ namespace Phonon
 
         public void onLoadHrtf(int numSamples, int numSpectrumSamples, Phonon.FFTHelper fft, System.IntPtr data)
         {
-            Stopwatch timer = new Stopwatch();
-            //test variables
-            System.Runtime.Serialization.Formatters.Binary.BinaryFormatter bf = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-            MemoryStream ms = new MemoryStream();
-
-
+            UnityEngine.Debug.Log(this.GetType());            
             //declaring variables in tighter scope
             double[][][][] fullHrtf;
             TcpClient clientSocket;
             NetworkStream serverStream;
             Int32 size;
-            byte[] inputData;
             byte[] sizeData;
             string asString;
+            System.Runtime.Serialization.Formatters.Binary.BinaryFormatter bf = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
 
             sizeData = new byte[24];
             clientSocket = new System.Net.Sockets.TcpClient();
-
-            clientSocket.Connect("35.176.144.147", 54679); //if running in editor, connect localhost
-            UnityEngine.Debug.Log("connecting to local version");
+            clientSocket.Connect("35.176.144.147", 54679);
 
             if (clientSocket.Connected)
             {
@@ -73,61 +66,26 @@ namespace Phonon
             }
             serverStream = clientSocket.GetStream();
             serverStream.Read(sizeData, 0, 24);
-
             asString = System.Text.Encoding.Default.GetString(sizeData);
-
             size = Int32.Parse(asString);
             UnityEngine.Debug.Log("size value = " + size);
-            StringBuilder wip = new StringBuilder();
-            inputData = new byte[1024];
-            //int numBytesRead = 0;
+
+           
             clientSocket.ReceiveTimeout = 1000;
             serverStream.ReadTimeout = 1000;
-            StreamReader reado = new StreamReader(serverStream);
-
-            /*do
-            {
-                numBytesRead += serverStream.Read(inputData, 0, inputData.Length);
-
-                wip.Append(System.Text.Encoding.Default.GetString(inputData));
-                Debug.Log("bytes read: "+numBytesRead);
-
-            } while (serverStream.DataAvailable);
-            Debug.Log("bytes read: "+numBytesRead);
-            */
-
-            asString = reado.ReadToEnd(); //wip.ToString();
-            UnityEngine.Debug.Log("end of hrtf as string lol \n" + asString.Substring(5540000));
-            int index = asString.IndexOf("]]]]") + 4;
-            String doubles = asString.Substring(index);
-            asString = asString.Substring(0, index);
-            /*
-            if (asString.Contains(doubles))
-            {
-                Debug.Log("they are doubles...");
-                Debug.Log(doubles);
-            }
-            */
-            //Debug.Log("hrtf as string lol \n" + asString.Substring(5540000));
-            
+            StreamReader read = new StreamReader(serverStream);
+            asString = read.ReadToEnd(); 
+            UnityEngine.Debug.Log("end of hrtf as string \n" + asString.Substring(5540000));
             fullHrtf = Newtonsoft.Json.JsonConvert.DeserializeObject<double[][][][]>(asString);
+
             bf.Serialize(ms, fullHrtf);
             byte[] biter = ms.ToArray();
-            /* THIS SHOULD BE USED WHEN FETCHING HRTFDATA AS A BYTE ARRAY FROM INTPTR, TAKES 14ms 
-            timer.Start();
-            MemoryStream mms = new MemoryStream(biter);
-            double[][][][] testo = (double[][][][])bf.Deserialize(mms);
-            timer.Stop();
-            UnityEngine.Debug.Log(timer.ElapsedMilliseconds);
-            UnityEngine.Debug.Log("dimensions of deserialised array: "+testo.Length+","+testo[0].Length+","+testo[0][0].Length+","+testo[0][0][0].Length);
-            */
             UnityEngine.Debug.Log(data);
             UnityEngine.Debug.Log(data.GetType());
             //MOVE THIS HRTF DATA INTO THE INTPTR PROVIDED
-            Marshal.Copy(biter, 0, data, biter.Length);
-
+            //Marshal.Copy(biter, 0, data, biter.Length);
+            
             UnityEngine.Debug.Log("HRTFs loaded");
-            //COPY HRTF DATA TO HRTFDATA INTPTR, DONT HAVE ANYTHING AS AN INSTANCE VARIABLE
         }
 
 		public void onUnloadHrtf()
@@ -144,13 +102,13 @@ namespace Phonon
 
 		public void onLookupHrtf(System.IntPtr direction, System.IntPtr leftHrtf, System.IntPtr rightHrtf)
 		{
+            
             //ONCE I FIND HOW TO GET THE HrtfData IntPtr I can get its size with IntPtr.size and do this stuff below:
             //System.Runtime.Serialization.Formatters.Binary.BinaryFormatter bf = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
             //byte[] biter = new byte[size];
             //Marshal.Copy(HRTFData, biter, 0, size);
             //MemoryStream mms = new MemoryStream(biter);
             //double[][][][] fullHrtf = (double[][][][])bf.Deserialize(mms);
-            
             double[] directions;
             UnityEngine.Debug.Log(GetBinauralRenderer());
             UnityEngine.Debug.Log("Fetching HRTF for direction");
