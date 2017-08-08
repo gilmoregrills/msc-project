f4217b524941fb9e8dacc30915cad65686e38da3
Ubuntu <ubuntu@ip-172-31-11-7.eu-west-2.compute.internal>
2017-08-08 15:59:09 +0000

    add capacity to lmdb, more logs from testing

--- implementation/individualiser/individualiser/lmdb_interface.py
+++ implementation/individualiser/individualiser/lmdb_interface.py
@@ -3,7 +3,7 @@ import numpy as np
 import cPickle as cpk
 
 def open():
-    env = lmdb.open("data/", map_size=4000011200, subdir=True)
+    env = lmdb.open("data/", map_size=8000011200, subdir=True)
     return env
 
 
