99bac2b4c07648b7b77d1615f1e10d8b38a65c2d
gilmoregrills <robinfarrowyonge@gmail.com>
2017-08-15 22:34:26 +0100

    generated subject charts, started difficult script!

--- implementation/individualiser/individualiser/analysis/all_errors_for_direction.py
+++ implementation/individualiser/individualiser/analysis/all_errors_for_direction.py
@@ -4,15 +4,40 @@
 import utils
 import sys
 import numpy as np 
+import os
 import scipy as scp 
 import matplotlib.pyplot as plot 
+import simplejson as json
 
 direction = int(sys.argv[1]) - 1# int, 1-8
 
-errors = utils.fetch_all_for_direction(direction)
-print errors.shape
+subjects = len(os.listdir("subjects"))
 
-errors = utils.sum_errors(errors)
-print errors.shape 
+output = []
 
-# now plot those onto something in matplotlib?
+for subject in range(1, subjects+1):
+	print "fetching data from subject: ", subject
+	path = "subjects/"+ str(subject) + ".json"
+	file = open(path, "r")
+	data = json.loads(file.read())["data"]
+	# now data from the subject is loaded into data
+	direction_data = []
+	for entry in data:
+		directo = entry['src_loc']
+		index = utils.DIRECTIONS.index(directo)
+		if index == direction:
+			direction_data.append(entry)
+	# now direction_data is a 6 index array 
+	for i in  range(0, len(direction_data)-1):
+		output.append({
+			"angle1": direction_data[i]['prcv_loc'],
+			"angle2": direction_data[i+1]['prcv_loc'],
+			"pcws1": direction_data[i]['pcws_before'],
+			"pcws2": direction_data[i+1]['pcws_before'],
+			"change": direction_data[i]['direction']
+			})
+		print i
+
+print len(output)
+print output[len(output)-1]
+
--- implementation/individualiser/individualiser/analysis/subject_error_rates.py
+++ implementation/individualiser/individualiser/analysis/subject_error_rates.py
@@ -9,11 +9,27 @@ import matplotlib.pyplot as plot
 
 # get that data
 subject = sys.argv[1]
-errors = utils.fetch_individual_errors(subject)
+output = utils.fetch_individual_errors(subject)
 
 # error is an angular thing, so sum those values
-errors = utils.sum_errors(errors)
+output = utils.sum_errors(output)
 
-print errors, errors.shape
+print output, output.shape
 
 #plot the data! 8 lines of 6 points!array
+fig = plot.figure()
+ax = plot.subplot(111)
+lines = ax.plot(output.transpose())
+for j in range(0, len(lines)):
+	print ", ".join(map(str, utils.DIRECTIONS[j]))
+	lines[j].set_label(" ".join(map(str, utils.DIRECTIONS[j])))
+#lines[j].set_label(" ".join(map(str, utils.DIRECTIONS[j])))
+print "number of lines", len(lines)
+box = ax.get_position()
+ax.set_position([box.x0, box.y0 + box.height * 0.15,
+                 box.width, box.height * 0.9])
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.09),
+          fancybox=True, shadow=True, ncol=5)
+plot.ylabel('Error (degrees)')
+plot.xlabel('Iterations')
+plot.show()
