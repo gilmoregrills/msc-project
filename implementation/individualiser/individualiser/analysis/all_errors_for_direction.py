1447a172c60725bdd42046ad3d71455c496f742c
gilmoregrills <robinfarrowyonge@gmail.com>
2017-08-16 14:34:42 +0100

    update thesis draft, add system diagram, work on PCW relationship

--- implementation/individualiser/individualiser/analysis/all_errors_for_direction.py
+++ implementation/individualiser/individualiser/analysis/all_errors_for_direction.py
@@ -10,13 +10,15 @@ import matplotlib.pyplot as plot
 import simplejson as json
 
 direction = int(sys.argv[1]) - 1# int, 1-8
+pair = int(sys.argv[2])
 
 subjects = len(os.listdir("subjects"))
 
 output = []
+for sublist in range(0, subjects):
+	output.append([])
 
 for subject in range(1, subjects+1):
-	print "fetching data from subject: ", subject
 	path = "subjects/"+ str(subject) + ".json"
 	file = open(path, "r")
 	data = json.loads(file.read())["data"]
@@ -29,15 +31,28 @@ for subject in range(1, subjects+1):
 			direction_data.append(entry)
 	# now direction_data is a 6 index array 
 	for i in  range(0, len(direction_data)-1):
-		output.append({
+		output[subject-1].append({
 			"angle1": direction_data[i]['prcv_loc'],
 			"angle2": direction_data[i+1]['prcv_loc'],
-			"pcws1": direction_data[i]['pcws_before'],
-			"pcws2": direction_data[i+1]['pcws_before'],
+			"pcws1": direction_data[i]['pcws_before'][0],
+			"pcws2": direction_data[i+1]['pcws_before'][0],
 			"change": direction_data[i]['direction']
 			})
-		print i
-
-print len(output)
-print output[len(output)-1]
-
+# now output should be roughly 10 * 5? :S debug pls! 
+# print len(output)
+# for j in range(0, subjects):
+# 	print len(output[j])
+print utils.DIRECTIONS[direction]
+for k in range(0, len(output)):
+	if pair < len(output[k]):
+		print "Printing data for subject: ", k+1
+		print "   Angle 1:"
+		print "   ", output[k][pair]['angle1']
+		print "   Angle 2:"
+		print "   ", output[k][pair]['angle2']
+		print "   PCW Set 1:"
+		print "   ", np.array(output[k][pair]['pcws1']).astype(np.int16)
+		print "   PCW Set 2:"
+		print "   ", np.array(output[k][pair]['pcws2']).astype(np.int16)
+		print "   Change directions!"
+		print "   ", output[k][pair]['change']
